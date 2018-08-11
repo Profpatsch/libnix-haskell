892a7c016bb1489bb0d38f1fa69f20d857a212c4
Profpatsch <mail@profpatsch.de>
2018-08-11 13:34:12 +0200

    Prefetch: add support for expected hash to `url`
    … and a few docstrings.

--- Foreign/Nix/Shellout/Prefetch.hs
+++ Foreign/Nix/Shellout/Prefetch.hs
@@ -6,15 +6,16 @@ module Foreign.Nix.Shellout.Prefetch
 , Url(..), Sha256(..)
 ) where
 
-import Protolude
-import Control.Error hiding (bool)
-import Data.Text (stripEnd, lines)
+import Protolude hiding (isPrefixOf)
+import Control.Error hiding (bool, err)
+import Data.Text (stripEnd, lines, isPrefixOf)
 
 import Foreign.Nix.Shellout.Types
 import qualified Foreign.Nix.Shellout.Helpers as Helpers
 
 data PrefetchError
   = PrefetchOutputMalformed Text
+  | ExpectedHashError
   | UnknownPrefetchError
   deriving (Eq, Show)
 
@@ -23,10 +24,17 @@ newtype Sha256 = Sha256 { unSha256 :: Text } deriving (Show, Eq, IsString)
 
 data UrlOptions = UrlOptions
   { urlUrl :: Url
+    -- ^ the URL
   , urlUnpack :: Bool
-  , urlName :: Maybe Text }
+    -- ^ whether to unpack before hashing (useful for prefetching @fetchTarball@)
+  , urlName :: Maybe Text
+    -- ^ name of the store path
+  , urlExpectedHash :: Maybe Sha256
+    -- ^ the hash we are expecting
+  }
 
 
+-- | Runs @nix-prefetch-url@.
 url :: UrlOptions -> NixAction PrefetchError (Sha256, StorePath Realized)
 url UrlOptions{..} = Helpers.readProcess handler exec args
   where
@@ -36,11 +44,15 @@ url UrlOptions{..} = Helpers.readProcess handler exec args
          <> [ "--type", "sha256"
             , "--print-path"
             , unUrl urlUrl ]
-    handler (out, _) = \case
+         <> maybe [] (pure.unSha256) urlExpectedHash
+    handler (out, err) = \case
       ExitSuccess -> withExceptT PrefetchOutputMalformed $ do
         let ls = lines $ stripEnd out
         path <- tryLast (exec <> " didn’t output a store path") ls
         sha  <- let errS = (exec <> " didn’t output a hash")
                 in tryLast errS =<< tryInit errS ls
         pure (Sha256 sha, StorePath $ toS path)
-      ExitFailure _ -> throwE UnknownPrefetchError
+      ExitFailure _ -> throwE $
+        if "error: hash mismatch" `isPrefixOf` err
+        then ExpectedHashError
+        else UnknownPrefetchError
