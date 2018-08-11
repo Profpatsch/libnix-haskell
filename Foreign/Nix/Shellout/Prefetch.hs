dfeb08a815f6422cdf35bff3bc51c9547babe21b
Profpatsch <mail@profpatsch.de>
2018-08-11 22:22:46 +0200

    Prefetch: create a wrapper for nix-prefetch-git
    This wraps most of the interface of that tool with (semi-sane) errors.

--- Foreign/Nix/Shellout/Prefetch.hs
+++ Foreign/Nix/Shellout/Prefetch.hs
@@ -1,14 +1,18 @@
-{-# LANGUAGE RecordWildCards, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards, GeneralizedNewtypeDeriving, ApplicativeDo #-}
 module Foreign.Nix.Shellout.Prefetch
 ( PrefetchError(..)
 , runNixAction, NixAction(..)
-, url, UrlOptions(..)
 , Url(..), Sha256(..)
+, url, UrlOptions(..), defaultUrlOptions
+, git, GitOptions(..), defaultGitOptions, GitOutput(..)
 ) where
 
-import Protolude hiding (isPrefixOf)
+import Protolude
 import Control.Error hiding (bool, err)
-import Data.Text (stripEnd, lines, isPrefixOf)
+import qualified Data.Text as T
+
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as AesonT
 
 import Foreign.Nix.Shellout.Types
 import qualified Foreign.Nix.Shellout.Helpers as Helpers
@@ -33,6 +37,13 @@ data UrlOptions = UrlOptions
     -- ^ the hash we are expecting
   }
 
+-- | Takes the URL, doesn’t unpack and uses the default name.
+defaultUrlOptions :: Url -> UrlOptions
+defaultUrlOptions u = UrlOptions
+  { urlUrl = u
+  , urlUnpack = False
+  , urlName = Nothing
+  , urlExpectedHash = Nothing }
 
 -- | Runs @nix-prefetch-url@.
 url :: UrlOptions -> NixAction PrefetchError (Sha256, StorePath Realized)
@@ -45,14 +56,100 @@ url UrlOptions{..} = Helpers.readProcess handler exec args
             , "--print-path"
             , unUrl urlUrl ]
          <> maybe [] (pure.unSha256) urlExpectedHash
+
     handler (out, err) = \case
       ExitSuccess -> withExceptT PrefetchOutputMalformed $ do
-        let ls = lines $ stripEnd out
+        let ls = T.lines $ T.stripEnd out
         path <- tryLast (exec <> " didn’t output a store path") ls
         sha  <- let errS = (exec <> " didn’t output a hash")
-                in tryLast errS =<< tryInit errS ls
+                in tryInit errS ls >>= tryLast errS
         pure (Sha256 sha, StorePath $ toS path)
       ExitFailure _ -> throwE $
-        if "error: hash mismatch" `isPrefixOf` err
+        if "error: hash mismatch" `T.isPrefixOf` err
+        then ExpectedHashError
+        else UnknownPrefetchError
+
+
+data GitOptions = GitOptions
+  { gitUrl :: Url
+    -- ^ the URL
+  , gitRev :: Maybe Text
+    -- ^ a git revision (hash, branch name, tag, ref, …)
+  , gitExpectedHash :: Maybe Sha256
+    -- ^ the hash we are expecting
+  , gitDeepClone :: Bool
+    -- ^ whether to do a deep instead of a shallow (@--depth=1@) git clone
+  , gitLeaveDotGit :: Bool
+    -- ^ whether to keep @.git@ directories
+  , gitFetchSubmodules :: Bool
+    -- ^ whether to fetch submodules
+  }
+
+-- | Takes the url, mirrors the default `fetchgit` options in nixpkgs:
+-- | no deep clone, no @.git@, fetches submodules by default.
+-- | By default, the latest default @rev@ is used.
+defaultGitOptions :: Url -> GitOptions
+defaultGitOptions u = GitOptions
+  { gitUrl = u
+  , gitRev = Nothing
+  , gitExpectedHash = Nothing
+  , gitDeepClone = False
+  , gitLeaveDotGit = False
+  , gitFetchSubmodules = True }
+
+data GitOutput = GitOutput
+  { gitOutputRev :: Text
+    -- ^ The actual revision that is used (useful if no 'gitRev' was given)
+  , gitOutputSha256 :: Sha256
+    -- ^ the hash
+  , gitOuputPath :: StorePath Realized
+  } deriving (Show, Eq)
+
+git :: GitOptions -> NixAction PrefetchError GitOutput
+git GitOptions{..} = Helpers.readProcess handler exec args
+  where
+    exec = "nix-prefetch-git"
+    args =  bool ["--no-deepClone"] ["--deepClone"] gitDeepClone
+         <> bool [] ["--leave-dotGit"] gitLeaveDotGit
+         <> bool [] ["--fetch-submodules"] gitFetchSubmodules
+         <> [ "--hash", "sha256" -- --hash is the type, not the thing
+            -- we need @url [rev [hash]]@,
+            -- otherwise we can’t expect a hash
+            , unUrl gitUrl
+            , maybe "" identity gitRev ]
+            -- hash comes last
+         <> maybe [] (\(Sha256 h) -> [h]) gitExpectedHash
+
+    handler (out, err) = \case
+      ExitSuccess -> withExceptT PrefetchOutputMalformed $ do
+        let error msg = exec <> " " <> msg
+            jsonError :: [Char] -> Text
+            jsonError = \msg -> error (T.intercalate "\n"
+                      [ "parsing json output failed:"
+                      , toS msg
+                      , "The output was:"
+                      , out ])
+
+        (gitOutputRev, gitOutputSha256)
+          <- ExceptT . pure . first jsonError $ do
+            val <- Aeson.eitherDecode' (toS out)
+            flip AesonT.parseEither val
+              $ Aeson.withObject "GitPrefetchOutput" $ \obj -> do
+                    (,) <$> obj Aeson..: "rev"
+                        <*> fmap Sha256 (obj Aeson..: "sha256")
+
+        -- The path isn’t output in the json, but on stderr. :(
+        -- So this is a bit more hacky than necessary.
+        gitOuputPath <- case
+          find ("path is /nix/store" `T.isPrefixOf`) (T.lines err)
+          >>= T.stripPrefix "path is " of
+          Nothing -> throwE
+            $ error "could not find nix store output path on stderr"
+          Just path -> pure $ StorePath $ toS path
+
+        pure GitOutput{..}
+
+      ExitFailure _ -> throwE $
+        if ("hash mismatch for URL" `T.isInfixOf` err)
         then ExpectedHashError
         else UnknownPrefetchError
